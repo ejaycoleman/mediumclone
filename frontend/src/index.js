774ea37b2b1b74d829197cad0ae5396faa64f1e1
ejaycoleman <ejaycoleman@googlemail.com>
2018-03-08 23:39:46 +0000

    added Material to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {darkBlack} from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Container from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: darkBlack,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+
+
+const App = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Container />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
