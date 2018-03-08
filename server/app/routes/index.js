0d7cd7235a8724d3729c8b09d2b44ab54221dc70
ejaycoleman <ejaycoleman@googlemail.com>
2018-03-08 23:18:51 +0000

    posts API started

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,10 +1,9 @@
 //const noteRoutes = require('./note_routes');
-const bookTranslationRoutes = require('./book_translation_routes');
-const bookContent = require('./book_content_routes');
+const posts = require('./post_routes');
 
 module.exports = function(app, db) {
   //noteRoutes(app, db);
-  bookTranslationRoutes(app, db);
-  bookContent(app, db);
+  posts(app, db);
+
   // Other route groups could go here, in the future
 };
